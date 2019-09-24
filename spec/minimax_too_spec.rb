6edb9a497a6261037b0920f6a67a14e2997a85e1
Kat <katrina@madetech.com>
2019-09-24 15:00:41 +0100

    refactored

--- spec/minimax_too_spec.rb
+++ spec/minimax_too_spec.rb
@@ -5,7 +5,7 @@ class Minimax
     result = tree[0]
 
     tree.each do |node|
-      node[:score] += score_of(node[:children], false)
+      node[:score] += score_of(node[:children])
       result = node if node[:score] > result[:score]
     end
     result[:position]
@@ -13,17 +13,25 @@ class Minimax
 
   private
 
-  def self.score_of(children, max)
+  def self.score_of(children, maximising = false)
     return 0 if children.empty?
 
-    child_score = children.map { |child| score_of(child[:children], !max) }.sum
+    child_score = children.map do |child|
+      score_of(child[:children], !maximising)
+    end.sum
 
-    child_score + min_or_max(children, max) { |c| c[:score] }[:score] - 1
+    child_score - 1 + min_or_max(children, maximising)
   end
 
-  def self.min_or_max(children, max, &block)
-    max ? children.max_by(&block) : children.min_by(&block)
+  def self.min_or_max(children, maximising)
+    if maximising
+      children.max_by { |c| c[:score] }[:score]
+    else
+      children.min_by { |c| c[:score] }[:score]
+    end
   end
+
+
 end
 
 describe Minimax do
